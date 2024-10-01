9cbacd56949daae21741df51e73e0be2467da660
nıdaturkay <145764089+NdaTrky@users.noreply.github.com>
2024-10-01 15:01:48 +0300

    Math objesi metotlarına çalışıldı.

---
+++ 29. MATH OBJESİ METOTLARI/index.js
@@ -0,0 +1,46 @@
+//! Math Sınıfı Metotları  - WİNDOW OBJESİ SINIFINDA BİR OBJE. 
+
+
+let sayi = 3.15;
+
+
+// * 1. FLOOR : Verilen değeri düşük olan sayıya yuvarlar.
+
+console.log(Math.floor(sayi));
+
+ // * 2. CEİL : Verilen değeri yüksek olan sayıya yuvarlar.
+
+console.log(Math.ceil(sayi));
+
+ // * 3. ROUND : Yuvarlamak anlamına gelir. En yakın değere yuvarlar.
+
+ console.log(Math.round(sayi));
+
+ // * 4. MAX : en büyük number değerini döner. 
+
+ console.log(Math.max(1,2,3,4,5,6))
+
+ // * 5. MİN : en kücük number değerini döner.
+
+ console.log(Math.min(1,2,3,4,5,6))
+
+ // * 6. RANDOM : Rastgele değerler üretir. 0 ile 1 arasında. 
+
+console.log(Math.floor(Math.random()*100)) ;
+
+ // * 7. ABS : Mutlak değeri almak için kullanılır. 
+
+console.log(Math.abs(sayi));
+
+ // * 8. SQRT : Karekökümü almak için kullanılır. 
+
+ console.log(Math.sqrt(sayi));
+
+ // * 9. POW : Üssünü (kuvvetini) almak için kullanılır.
+
+ console.log(Math.pow(3,8));
+
+
+ // * 10. Pİ SAYISI: 
+
+ console.log(Math.PI);
