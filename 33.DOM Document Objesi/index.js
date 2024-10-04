69fadb44cf026ae022ef8a6f1adc2e62e9680ceb
nıdaturkay <145764089+NdaTrky@users.noreply.github.com>
2024-10-04 16:01:53 +0300

    document objesi calısıldı

---
+++ 33.DOM Document Objesi/index.js
@@ -0,0 +1,20 @@
+// ! Document Objesi
+
+// * JavaScript'te document objesi, web sayfasındaki içeriğe ve yapıya erişmek için kullanılan bir nesnedir. Bu nesne, Document Object Model (DOM) aracılığıyla HTML belgesini temsil eder ve sayfa üzerinde etkileşimde bulunmanıza, elemanları manipüle etmenize ve dinamik içerik oluşturmanıza olanak tanır.
+
+console.log(window);
+
+console.log(document.location);
+
+value = document.location.href;
+value1 = document.location.hostname;
+value2= document.location.port;
+value3 = document.location.pathname;
+
+value4= document.characterSet;
+
+console.log(value);
+console.log(value1);
+console.log(value2);
+console.log(value3);
+console.log(value4);
