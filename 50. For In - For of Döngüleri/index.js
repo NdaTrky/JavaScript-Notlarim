b7e5937b9aedcf42c2fb00efc77f501c7a5f7c3f
nıdaturkay <145764089+NdaTrky@users.noreply.github.com>
2024-10-19 15:25:04 +0300

    for in and for of loop

--- 50. For In - For of Döngüleri/index.js
+++ 50. For In - For of Döngüleri/index.js
@@ -1,3 +1,40 @@
 // ! For in And For Of Loop 
 
 
+// ? For in döngüsü, her elemanın İNDEX değerini verir. 
+//? Bunu (in) ile aklımızda tutabiliriz. Bu index'in kısatlması olarak düşünülebilir. 
+
+
+
+
+let names = ["nida", "semih","burak","merve","ali"]
+
+for(let name in names){
+    console.log(name) 
+}
+
+//eğer; 
+
+for(let name in names){
+    console.log(name , names[name]) //şeklinde yazarsak, isim değerlerini de index değerinin yanına yazdırmış oluruz.
+}
+
+
+
+// ? For Of Loop --- Direkt değeri verir. indexOf ile hem değerine hem indexine erişebiliriz. 
+
+for(let isim of names){
+    console.log(isim, names.indexOf(isim));
+}
+
+
+
+
+
+
+
+
+
+
+
+
